48468064940f2be16ce9983c4c554c18ce2ed904
davidadediji <davidadediji@gmail.com>
2022-07-22 13:25:16 +0100

    [feat]-last changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,9 @@
 import '../index.css'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import {ModelInterface} from '../model-interface'
+
+
+
 const Card = ({cards}:ModelInterface)=>{
     return(
         <div className="card-container">
@@ -10,6 +14,13 @@ const Card = ({cards}:ModelInterface)=>{
                         <img src={card.image} alt="travel-location" />
                     </div>
                     <div className='travel-details'>
+                        <div className='travel-details-country'>
+                            <p className='top-country'><span ><LocationOnIcon className='location-img'/></span><span>{card.country}</span></p>
+                            <p className='map-ins'>View on Google Maps</p>
+                        </div>
+                        <p className='card-location'>{card.location}</p>
+                        <p className='card-date'>{card.startdate} - {card.enddate}</p>
+                        <p className='card-description'>{card.description}</p>
                     </div>
                     </div>
                 )})
